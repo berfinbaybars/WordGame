5bdfacf1fa53f5403e372870dfc7c7a52fe5b8b0
Berfin <berfinbaybars@hotmail.com>
2022-01-11 02:48:07 +0300

    Keyboard Added

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,10 +7,10 @@ using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
-    int count = 0;
-    int currentRow = 1;
-    bool addChar = true;
-    bool playable = true;
+    static int count = 0;
+    static int currentRow = 1;
+    static bool addChar = true;
+    static bool playable = true;
     [SerializeField] TextAsset textAsset;
     List<string> WordList;
     string answer;
@@ -20,6 +20,8 @@ public class Game : MonoBehaviour
     [SerializeField] Text AnswerText;
     [SerializeField] Button NextButton;
     [SerializeField] Button RestartButton;
+    [SerializeField] Button EnterButton;
+    [SerializeField] Button DeleteButton;
     [SerializeField] GameObject GameOver;
 
     void Start()
@@ -31,6 +33,8 @@ public class Game : MonoBehaviour
         WordList = new List<string>(words);
         NextButton.onClick.AddListener(NextWord);
         RestartButton.onClick.AddListener(RestartGame);
+        EnterButton.onClick.AddListener(KeyboardEnter);
+        DeleteButton.onClick.AddListener(KeyboardDelete);
         NextButton.gameObject.SetActive(false);
         ChooseWord();
         playable = true;
@@ -139,6 +143,97 @@ public class Game : MonoBehaviour
         }
     }
 
+    public static void AddChar(char ch)
+    {
+        if (count > 0 && count % 5 == 0 && count == currentRow * 5)
+        {
+            addChar = false;
+        }
+        else
+        {
+            addChar = true;
+        }
+
+        if (addChar && count < 30)
+        {
+            count++;
+            TextDictionary.TextObjs[count].text = ch.ToString();
+        }
+    }
+
+    void KeyboardDelete()
+    {
+        if (count > ((currentRow * 5) - 5) && count <= (currentRow * 5))
+        {
+            TextDictionary.TextObjs[count].text = "";
+            count--;
+            ErrorText.gameObject.SetActive(false);
+        }
+    }
+
+    void KeyboardEnter()
+    {
+        if(count != (currentRow * 5))
+        {
+            return;
+        }
+        string enteredWord = "";
+        int firstIndex = count - 4;
+        for (int i = firstIndex; i <= count; i++)
+        {
+            string temp = TextDictionary.TextObjs[i].text;
+            if (temp == "I")
+            {
+                temp = "ı";
+            }
+            else
+            {
+                temp = temp.ToLower();
+            }
+            enteredWord += temp;
+        }
+
+        bool exist = false;
+        WordList.ForEach(word =>
+        {
+            if (word.Contains(enteredWord))
+            {
+                exist = true;
+            }
+        });
+
+        if (!exist)
+        {
+            ErrorText.gameObject.SetActive(true);
+            return;
+        }
+
+        char[] charArray = enteredWord.ToCharArray();
+
+        for (int k = 0; k < charArray.Length; k++)
+        {
+            char ch = charArray[k];
+
+            CheckChar(ch, k, (firstIndex + k), enteredWord);
+
+        }
+
+        if (answer.Equals(enteredWord))
+        {
+            Win();
+        }
+        else if (currentRow < 6)
+        {
+            currentRow++;
+        }
+        else
+        {
+            SetAnswerText(answer);
+            playable = false;
+            GameOver.SetActive(true);
+        }
+    }
+
 
     void ChooseWord()
     {
@@ -151,9 +246,24 @@ public class Game : MonoBehaviour
         char answerChar = answer.ToCharArray()[checkIndex];
         GameObject panel = PanelDictionary.PanelObjs[panelIndex];
 
+        char getKeyboard;
+        if(ch == 'i')
+        {
+            getKeyboard = 'İ';
+        }
+        else
+        {
+            getKeyboard = char.ToUpper(ch);
+        }
+        
+        Button keyboard = KeyboardDictionary.KeyboardObjs[getKeyboard];
+        Color color = keyboard.GetComponent<Image>().color;
+        string colorName = CheckColor(color);
+        
         if (answerChar == ch)
         {
             panel.GetComponent<Image>().color = new Color(0, 255, 0); // Green
+            keyboard.GetComponent<Image>().color = new Color(0, 255, 0); // Green
         }
         else if (answer.Contains(ch.ToString()))
         {
@@ -162,26 +272,45 @@ public class Game : MonoBehaviour
                 if (enteredWord.ToCharArray()[answer.IndexOf(ch)] == ch)
                 {
                     panel.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f); // Gray
+                    if(colorName != "yellow" && colorName != "green")
+                    {
+                        keyboard.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f); // Gray
+                    }
                     return;
                 }
                 panel.GetComponent<Image>().color = new Color(255, 255, 0); // Yellow
-
+                if (colorName != "green")
+                {
+                    keyboard.GetComponent<Image>().color = new Color(255, 255, 0); // Yellow
+                }
             }
             else
             {
                 if (ControlIndex(answer, enteredWord, ch))
                 {
                     panel.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f); // Gray
+                    if (colorName != "yellow" && colorName != "green")
+                    {
+                        keyboard.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f); // Gray
+                    }
                 }
                 else
                 {
                     panel.GetComponent<Image>().color = new Color(255, 255, 0); // Yellow
+                    if (colorName != "green")
+                    {
+                        keyboard.GetComponent<Image>().color = new Color(255, 255, 0); // Yellow
+                    }
                 }
             }
         }
         else
         {
             panel.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f); // Gray
+            if (colorName != "yellow" && colorName != "green")
+            {
+                keyboard.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f); // Gray
+            }
         }
     }
 
@@ -209,6 +338,7 @@ public class Game : MonoBehaviour
     {
         NextButton.gameObject.SetActive(false);
         ResetTable();
+        ResetKeyboard();
         count = 0;
         currentRow = 1;
         addChar = true;
@@ -219,6 +349,7 @@ public class Game : MonoBehaviour
     void RestartGame()
     {
         ResetTable();
+        ResetKeyboard();
         count = 0;
         currentRow = 1;
         score = 0;
@@ -270,6 +401,36 @@ public class Game : MonoBehaviour
                 string temp = charArray[i].ToString();
                 AnswerText.text += temp.ToUpper();
             }
+        }
+    }
+
+    void ResetKeyboard()
+    {
+        List<char> keyList = new List<char>(KeyboardDictionary.KeyboardObjs.Keys);
+        for(int i = 0; i < keyList.Count; i++)
+        {
+            char ch = keyList[i];
+            Button keyboard = KeyboardDictionary.KeyboardObjs[ch];
+            keyboard.GetComponent<Image>().color = new Color(255, 255, 255);
+        }
+
+    }
+
+    string CheckColor(Color color)
+    {
+        int hashCode = color.GetHashCode();
+        switch (hashCode)
+        {
+            case -778043392:
+                return "white";
+            case -754974720:
+                return "gray";
+            case 1363345408:
+                return "yellow";
+            case 305922048:
+                return "green";
+            default:
+                return "white";
         }
     }
 }
---
+++ Assets/Scripts/KeyboardDictionary.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardDictionary : MonoBehaviour
+{
+    public static Dictionary<char, Button> KeyboardObjs = new Dictionary<char, Button>();
+    [SerializeField] Button Button1;
+    [SerializeField] Button Button2;
+    [SerializeField] Button Button3;
+    [SerializeField] Button Button4;
+    [SerializeField] Button Button5;
+    [SerializeField] Button Button6;
+    [SerializeField] Button Button7;
+    [SerializeField] Button Button8;
+    [SerializeField] Button Button9;
+    [SerializeField] Button Button10;
+    [SerializeField] Button Button11;
+    [SerializeField] Button Button12;
+    [SerializeField] Button Button13;
+    [SerializeField] Button Button14;
+    [SerializeField] Button Button15;
+    [SerializeField] Button Button16;
+    [SerializeField] Button Button17;
+    [SerializeField] Button Button18;
+    [SerializeField] Button Button19;
+    [SerializeField] Button Button20;
+    [SerializeField] Button Button21;
+    [SerializeField] Button Button22;
+    [SerializeField] Button Button23;
+    [SerializeField] Button Button24;
+    [SerializeField] Button Button25;
+    [SerializeField] Button Button26;
+    [SerializeField] Button Button27;
+    [SerializeField] Button Button28;
+    [SerializeField] Button Button29;
+    [SerializeField] Button Button30;
+    [SerializeField] Button Button31;
+    [SerializeField] Button Button32;
+
+    bool isReady = false;
+    void Start()
+    {
+        if (KeyboardObjs.Count == 0)
+        {
+            KeyboardObjs.Add('Q', Button1);
+            KeyboardObjs.Add('W', Button2);
+            KeyboardObjs.Add('E', Button3);
+            KeyboardObjs.Add('R', Button4);
+            KeyboardObjs.Add('T', Button5);
+            KeyboardObjs.Add('Y', Button6);
+            KeyboardObjs.Add('U', Button7);
+            KeyboardObjs.Add('I', Button8);
+            KeyboardObjs.Add('O', Button9);
+            KeyboardObjs.Add('P', Button10);
+            KeyboardObjs.Add('Ğ', Button11);
+            KeyboardObjs.Add('Ü', Button12);
+            KeyboardObjs.Add('A', Button13);
+            KeyboardObjs.Add('S', Button14);
+            KeyboardObjs.Add('D', Button15);
+            KeyboardObjs.Add('F', Button16);
+            KeyboardObjs.Add('G', Button17);
+            KeyboardObjs.Add('H', Button18);
+            KeyboardObjs.Add('J', Button19);
+            KeyboardObjs.Add('K', Button20);
+            KeyboardObjs.Add('L', Button21);
+            KeyboardObjs.Add('Ş', Button22);
+            KeyboardObjs.Add('İ', Button23);
+            KeyboardObjs.Add('Z', Button24);
+            KeyboardObjs.Add('X', Button25);
+            KeyboardObjs.Add('C', Button26);
+            KeyboardObjs.Add('V', Button27);
+            KeyboardObjs.Add('B', Button28);
+            KeyboardObjs.Add('N', Button29);
+            KeyboardObjs.Add('M', Button30);
+            KeyboardObjs.Add('Ö', Button31);
+            KeyboardObjs.Add('Ç', Button32);
+        }
+        isReady = true;
+    }
+
+    private void Update()
+    {
+        if (isReady)
+        {
+            AddEvents();
+            isReady = false;
+        }
+    }
+    void AddEvents()
+    {
+        List<char> keyList = new List<char>(KeyboardObjs.Keys);
+        for (int i = 0; i < keyList.Count; i++)
+        {
+            char ch = keyList[i];
+            Button keyboard = KeyboardObjs[ch];
+            keyboard.onClick.AddListener(() => Game.AddChar(ch));
+        }
+    }
+}
